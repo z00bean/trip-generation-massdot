b5333fec665a11a0a0d74841b57e40650dc3d9b2
Zubin Bhuyan <18747679+z00bean@users.noreply.github.com>
2024-02-28 21:07:48 -0500

    Update main_job-1.sh

--- yv8-trk-src/main_job-1.sh
+++ yv8-trk-src/main_job-1.sh
@@ -9,10 +9,10 @@
 pwd
 module load miniconda
 conda init bash
-conda activate /home/zubin_bhuyan_student_uml_edu/.conda/envs/yolov8-track
+conda activate yolov8-track
 nvidia-smi
 echo
 echo
-cd /work/pi_yuanchang_xie_uml_edu/zubin/CODE/YOLOv8-DeepSORT/yolo/v8/detect
+cd ~/CODE/YOLOv8-DeepSORT/yolo/v8/detect
 echo
 sh B1-374E-tracking-veh.sh
